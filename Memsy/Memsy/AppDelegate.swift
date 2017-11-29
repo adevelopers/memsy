2ec346ee074fd027b0baf11a2a28e85dfe66f4f1
Kirill Khudyakov <hudiakov@gmail.com>
2017-11-29 14:04:49 +0300

    Init project

---
+++ Memsy/Memsy/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Memsy
+//
+//  Created by Kirill Khudyakov on 29.11.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+  
+        return true
+    }
+
+}
---
+++ Memsy/Memsy/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Memsy
+//
+//  Created by Kirill Khudyakov on 29.11.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+}
