80b60550eafe57dda89a26b37fe7555b48e75b70
Alexey Gulyaev <sargonmeister@gmail.com>
2017-12-30 12:29:27 +0300

    add print coordinate and this is workable code

--- Memsy/Memsy/ViewController.swift
+++ Memsy/Memsy/ViewController.swift
@@ -21,6 +21,18 @@ class ViewController: UIViewController {
         
         memImageView.image = #imageLiteral(resourceName: "mem2")
         memImageView.contentMode = .scaleAspectFit
+        
+        let Mwidth = UIScreen.main.bounds.width
+        let Mheight = UIScreen.main.bounds.height
+        print(Mwidth, Mheight)
+        
+        let red = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        
+        red.backgroundColor = .red
+        red.center = CGPoint(x: 251, y: 508)
+        self.view.addSubview(red)
+        
     }
 
 }
