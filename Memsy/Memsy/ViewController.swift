6deb6dd26ec7a83c92031ebfa3c4c2576b6d0be3
Alexey Gulyaev <sargonmeister@gmail.com>
2018-01-14 21:08:19 +0300

    1. we add add pod Snapkit 2. rechange coordinate buttons with him 3. add gradient from background 4. add shadows from my objects in main screen 5. we kept an old code so far, without snap kit

--- Memsy/Memsy/ViewController.swift
+++ Memsy/Memsy/ViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import SnapKit
+
+class AppColors {
+    static let firstColor: UIColor = #colorLiteral(red: 0.962474525, green: 0.9660497308, blue: 0.9782558084, alpha: 1)
+}
+
 
 class ViewController: UIViewController {
     
@@ -14,13 +20,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var memImageView: UIImageView!
     
     var gradientView: UIView!
+    
     var gradientLayer:CAGradientLayer!
     var nopeButton: UIButton!
     var rewindButton: UIButton!
     
+    
     let mainWidth = UIScreen.main.bounds.width
     let mainHeight = UIScreen.main.bounds.height
     let buttonSize: CGFloat = 50.0
+    
+ 
+   
     
     @IBAction func refreshButtonAction(_ sender: Any) {
         print("Картинка обновилась")
@@ -28,99 +39,133 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("\(#function)")
         
+        //        работаем с мемконтейнер
+        memContainer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        memContainer.layer.cornerRadius = 10
+        
+        //        работаем с тенью мемконтейнера
+        memContainer.layer.shadowOffset = CGSize(width: 0, height: 5)
+        memContainer.layer.shadowOpacity = 0.5
+        memContainer.layer.shadowRadius = 2
+        
+        //        работаем с картинкой в мемконтейнере
         memImageView.image = #imageLiteral(resourceName: "mem2")
         memImageView.contentMode = .scaleAspectFit
         
+//        работаем с кнопкой #imageLiteral(resourceName: "nope")
         nopeButton = UIButton(frame: CGRect.zero)
         let nopeImage = UIImage(named: "nope")!.withRenderingMode(.alwaysOriginal)
         nopeButton.setImage(nopeImage, for: .normal)
         nopeButton.contentMode = .scaleAspectFit
         nopeButton.layer.cornerRadius = buttonSize / 2
-        nopeButton.layer.masksToBounds = true
+        nopeButton.layer.masksToBounds = false
         nopeButton.backgroundColor = UIColor.white
+        
+        //        работаем с тенью nopeButton
+        nopeButton.layer.shadowOffset = CGSize(width: 0, height: 5)
+        nopeButton.layer.shadowOpacity = 0.5
+        nopeButton.layer.shadowRadius = 2
+        
+        
         view.addSubview(nopeButton)
         
-        
+//        работаем с кнопкой #imageLiteral(resourceName: "rewind")
         rewindButton = UIButton(frame: CGRect.zero)
         let rewindImage = UIImage(named: "rewind")!.withRenderingMode(.alwaysOriginal)
         rewindButton.setImage(rewindImage, for: .normal)
         rewindButton.contentMode = .scaleAspectFill
         rewindButton.layer.cornerRadius = buttonSize / 2
-        rewindButton.layer.masksToBounds = true
+        rewindButton.layer.masksToBounds = false
         rewindButton.backgroundColor = UIColor.white
+        
+        //        работаем с тенью rewindButton
+        rewindButton.layer.shadowOffset = CGSize(width: 0, height: 5)
+        rewindButton.layer.shadowOpacity = 0.5
+        rewindButton.layer.shadowRadius = 2
+        
         view.addSubview(rewindButton)
         
-//        gradientView.backgroundColor = UIColor.gray
-        
+//        работаем с градиентом основного фона
         gradientLayer = CAGradientLayer()
-//        view.layer.insertSublayer(gradientLayer, at: 0)
-        
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 0	, y: 1)
-        
-        let startColor = UIColor.white.cgColor
-        let endColor = UIColor(red: 243.0 / 255.0, green: 244.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
-        
-        gradientLayer.colors = [startColor, endColor]
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.frame = UIScreen.main.bounds
+        gradientLayer.colors = [AppColors.firstColor.cgColor, UIColor.gray.cgColor]
         
         view.layer.insertSublayer(gradientLayer, at: 0)
         
-//        gradientLayer.locations = []
+        // constrains кнопок
+        
+        //        nopeButton
+        nopeButton.snp.makeConstraints { set in
+            set.width.height.equalTo(50)
+            set.right.equalTo(view).offset(-19)
+            set.bottom.equalTo(view).offset(-10)
+        }
+        //        rewindButton
+        rewindButton.snp.makeConstraints { set in
+            set.width.height.equalTo(50)
+            set.left.equalTo(view).offset(23)
+            set.bottom.equalTo(view).offset(-10)
+        }
     }
     
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
-//        //получаем максимальную координату по ширине и высоте
-//        let mainWidth = UIScreen.main.bounds.width
-//        let mainHeight = UIScreen.main.bounds.height
-//
-//        //находим коэффициент на который надо умножить для нового разрешения
-//        let CoefX = 320 / mainWidth
-//        let CoefY = 568 / mainHeight
-//
-//        // рисуем нашу nope кнопку согласно нового разрешения
-//        let nopeButtonFrame = CGRect(
-//            x: 251 * CoefX,
-//            y: 508 * CoefY,
-//            width: 50 * CoefX,
-//            height: 50 * CoefY)
-//        nopeButton.frame = nopeButtonFrame
-//
-//        // рисуем нашу rewind кнопку согласно нового разрешения
-//        let rewindFrame = CGRect(
-//            x: 23 * CoefX,
-//            y: 508 * CoefY,
-//            width: 50 * CoefX,
-//            height: 50 * CoefY)
-//        rewindButton.frame = rewindFrame
+        print("\(#function)")
+        
+        //        //получаем максимальную координату по ширине и высоте
+        //        let mainWidth = UIScreen.main.bounds.width
+        //        let mainHeight = UIScreen.main.bounds.height
+        //
+        //        //находим коэффициент на который надо умножить для нового разрешения
+        //        let CoefX = 320 / mainWidth
+        //        let CoefY = 568 / mainHeight
+        //
+        //        // рисуем нашу nope кнопку согласно нового разрешения
+        //        let nopeButtonFrame = CGRect(
+        //            x: 251 * CoefX,
+        //            y: 508 * CoefY,
+        //            width: 50 * CoefX,
+        //            height: 50 * CoefY)
+        //        nopeButton.frame = nopeButtonFrame
+        //
+        //        // рисуем нашу rewind кнопку согласно нового разрешения
+        //        let rewindFrame = CGRect(
+        //            x: 23 * CoefX,
+        //            y: 508 * CoefY,
+        //            width: 50 * CoefX,
+        //            height: 50 * CoefY)
+        //        rewindButton.frame = rewindFrame
         
         //получаем максимальную координату по ширине и высоте
-//        myGradientLayer.startPoint = CGPoint(x:0, y:0)
-//        myGradientLayer.endPoint = CGPoint(x:0, y:1)
+        //        myGradientLayer.startPoint = CGPoint(x:0, y:0)
+        //        myGradientLayer.endPoint = CGPoint(x:0, y:1)
         
         
-        gradientLayer.frame = view.bounds
-        
-        // рисуем нашу nope кнопку согласно нового разрешения
-        let nopeButtonFrame = CGRect(
-            x: memContainer.frame.origin.x,
-            y: mainHeight - buttonSize - 10,
-            width: buttonSize,
-            height: buttonSize)
-        nopeButton.frame = nopeButtonFrame
-        
-        
-        // рисуем нашу rewind кнопку согласно нового разрешения
-        let rewindFrame = CGRect(
-            x: memContainer.frame.origin.x +
-                memContainer.frame.size.width - buttonSize,
-            y: mainHeight - buttonSize - 10,
-            width: buttonSize,
-            height: buttonSize)
-        rewindButton.frame = rewindFrame
+        //        gradientLayer.frame = self.view.bounds
+        //
+        //        print(view.bounds)
+        //        // рисуем нашу nope кнопку согласно нового разрешения
+        //        let nopeButtonFrame = CGRect(
+        //            x: memContainer.frame.origin.x,
+        //            y: mainHeight - buttonSize - 10,
+        //            width: buttonSize,
+        //            height: buttonSize)
+        //        nopeButton.frame = nopeButtonFrame
+        //
+        //
+        //        // рисуем нашу rewind кнопку согласно нового разрешения
+        //        let rewindFrame = CGRect(
+        //            x: memContainer.frame.origin.x +
+        //                memContainer.frame.size.width - buttonSize,
+        //            y: mainHeight - buttonSize - 10,
+        //            width: buttonSize,
+        //            height: buttonSize)
+        //        rewindButton.frame = rewindFrame
     }
 }
 
